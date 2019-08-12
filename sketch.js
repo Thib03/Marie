bc40b992b60ba211a825af05ba2cc0d966985b12
Thib03 <41568449+Thib03@users.noreply.github.com>
2019-08-12 20:28:18 +0200

    font

--- sketch.js
+++ sketch.js
@@ -1,15 +1,48 @@
+//------------------------------------------------------------------------------
+//              Variables
+//------------------------------------------------------------------------------
+
+var homeButtons = [];
+  var solfegeButtons = [];
+    var aleatoireButtons = [];
+    var gammeButtons = [];
+    var rythmeButtons = [];
+  var cartesButtons = [];
+  var lutherieButtons = [];
+  var compositeursButtons = [];
+
+var manager;
+
+var clefSol;
+
+var yt;
+
+var solfegeMenu      = [0,[0,0,0],0];
+var cartesMenu       = [0,0];
+var lutherieMenu     = [0,0];
+var compositeursMenu = [0];
+
+var black = 87;
+
+var font;
+
+//------------------------------------------------------------------------------
+//              Classes
+//------------------------------------------------------------------------------
+
 class Button {
   constructor(t,/*i,*/x,y,l,h) {
     this.button = new Clickable(width /2+x-l/2,
                                 height/2+y-h/2);
     this.button.resize(l,h);
-    this.button.color = 255;
-    this.button.cornerRadius = 0;
-    this.button.strokeWeight = 2;
+    this.button.color = 217;
+    this.button.cornerRadius = 5;
+    this.button.strokeWeight = 0;
     this.button.stroke = 0;
     this.button.text = '';
 
     this.t = t;
+    this.tc = black;
     //this.i = i;
     this.x = x;
     this.y = y;
@@ -30,7 +63,7 @@ class Button {
   }
 
   drawText() {
-    fill(0);
+    fill(this.tc);
     textAlign(CENTER,CENTER);
     textSize(18);
     text(this.t,width/2+this.x,
@@ -42,40 +75,101 @@ class Button {
     this.drawText();
     //image(this.i,0,0,50,200);
   }
+
+  toggle(bool) {
+    if(bool) {
+      this.button.color = black;
+      this.tc = 217;
+    }
+    else {
+      this.button.color = 217;
+      this.tc = black;
+    }
+  }
 }
 
-var homeButtons = [];
-var solfegeButtons = [];
-var cartesButtons = [];
-var lutherieButtons = [];
-var compositeursButtons = [];
+//------------------------------------------------------------------------------
+//              Functions
+//------------------------------------------------------------------------------
 
-var manager;
+function drawPortee() {
+  noStroke();
+  fill(255);
 
-var clefSol;
+  let y = height / 2 + 1.6 * dy;
+
+  rect(0, y - 5 * marge, width, 10 * marge);
+
+  stroke(noir);
+  strokeWeight(marge / 10);
+  line(marge, y - 2 * marge,
+    width - marge, y - 2 * marge);
+  line(marge, y - marge,
+    width - marge, y - marge);
+  line(marge, y,
+    width - marge, y);
+  line(marge, y + marge,
+    width - marge, y + marge);
+  line(marge, y + 2 * marge,
+    width - marge, y + 2 * marge);
+
+  imageMode(CENTER);
+  switch (clef) {
+    case 0:
+      image(clefSol, 3 * marge, y + marge / 6,
+        117 / 25 * marge, 200 / 25 * marge);
+      break;
+    case 1:
+      image(clefFa, 3 * marge, y - 0.35 * marge,
+        180 / 59 * marge, 200 / 59 * marge);
+      break;
+    case 2:
+      image(clefUt, 3 * marge, y,
+        135 / 50 * marge, 200 / 50 * marge);
+  }
+}
 
 function preload() {
   //clefSol = loadImage('clef_sol.png');
   //clefSol = createImg('https://ibb.co/LQKk4N1');
+  font = loadFont('font.otf');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
+  textFont(font);
+
   manager = new SceneManager();
 
   manager.addScene(homeScene);
-  manager.addScene(solfegeScene);
-  manager.addScene(cartesScene);
-  manager.addScene(lutherieScene);
-  manager.addScene(compositeursScene);
-
+    manager.addScene(solfegeMenuScene);
+    manager.addScene(solfegeGameScene);
+    manager.addScene(cartesScene);
+    manager.addScene(lutherieScene);
+    manager.addScene(compositeursScene);
   manager.showScene(homeScene);
 
-  let e = 20;
-  let y = height/8;
-  let l = (width-5*e)/4;
-  let h = height/5;
+  let e = 40;
+  let y0 = -(height/5+e)/2;
+  let y1 = (height/5+e)/2;
+  let y = y0;
+  var l;
+  function largeur(n) {
+    l = (width-(n+1)*e)/n;
+  }
+  var h;
+  function hauteur(n) {
+    h = (height/5-(n-1)*e)/n;
+  }
+
+  //let
+
+  let yr = height/2.6;
+  let lr = (width-4*e)/3;
+  let hr = h;
+
+  yt = height/8.5;
 
   var button;
 
@@ -83,25 +177,28 @@ function setup() {
   //                            Home Buttons
   //----------------------------------------------------------------------------
 
-  button = new Button('Solfège',    -3*(l+e)/2,y,l,h);
+  largeur(2);
+  hauteur(1);
+
+  button = new Button('Solfège',    -(l+e)/2,y0,l,h);
   button.button.onPress = function() {
-      manager.showScene(solfegeScene);
+      manager.showScene(solfegeMenuScene);
   }
   homeButtons.push(button);
 
-  button = new Button('Cartes',     -  (l+e)/2,y,l,h);
+  button = new Button('Cartes',      (l+e)/2,y0,l,h);
   button.button.onPress = function() {
       manager.showScene(cartesScene);
   }
   homeButtons.push(button);
 
-  button = new Button('Lutherie',      (l+e)/2,y,l,h);
+  button = new Button('Lutherie',      -(l+e)/2,y1,l,h);
   button.button.onPress = function() {
       manager.showScene(lutherieScene);
   }
   homeButtons.push(button);
 
-  button = new Button('Compositeurs',3*(l+e)/2,y,l,h);
+  button = new Button('Compositeurs', (l+e)/2,y1,l,h);
   button.button.onPress = function() {
       manager.showScene(compositeursScene);
   }
@@ -111,39 +208,165 @@ function setup() {
   //                           Solfège Buttons
   //----------------------------------------------------------------------------
 
-  l = (width-4*e)/3;
+  largeur(3);
 
-  button = new Button('Retour',    0,height/2.8,l,h/3);
+  function update(buttons,state,first=0,last=buttons.length-1) {
+    for(let b = first; b <= last; b++) {
+      if(b == state+first) {
+        buttons[b].toggle(true);
+      }
+      else {
+        buttons[b].toggle(false);
+      }
+    }
+  }
+
+  button = new Button('Aléatoire',   -(l+e),y0,l,h);
+  button.toggle(true);
   button.button.onPress = function() {
-      manager.showScene(homeScene);
+    solfegeMenu[0] = 0;
+    update(solfegeButtons,0);
   }
   solfegeButtons.push(button);
 
-  button = new Button('Aléatoire',   -(l+e),y,l,h);
+  button = new Button('Gamme',    0,y0,l,h);
   button.button.onPress = function() {
-      //manager.showScene(homeScene);
+    solfegeMenu[0] = 1;
+    update(solfegeButtons,1);
   }
   solfegeButtons.push(button);
 
-  button = new Button('Gamme',    0,y,l,h);
+  button = new Button('Rythme',    l+e,y0,l,h);
   button.button.onPress = function() {
-      //manager.showScene(homeScene);
+    solfegeMenu[0] = 2;
+    update(solfegeButtons,2);
   }
   solfegeButtons.push(button);
 
-  button = new Button('Rythme',    l+e,y,l,h);
+  button = new Button('Retour',    -(l+e),yr,l,h/3);
   button.button.onPress = function() {
-      //manager.showScene(homeScene);
+    manager.showScene(homeScene);
   }
   solfegeButtons.push(button);
+
+  button = new Button('Jouer',    l+e,yr,l,h/3);
+  button.button.onPress = function() {
+    manager.showScene(solfegeGameScene);
+  }
+  solfegeButtons.push(button);
+
+  //----------------------------------------------------------------------------
+  //                           Aléatoire Buttons
+  //----------------------------------------------------------------------------
+
+  button = new Button('Aucune\nrestriction',   -(l+e),y1,l,h);
+  button.toggle(true);
+  button.button.onPress = function() {
+    solfegeMenu[1][0] = 0;
+    update(aleatoireButtons,0);
+  }
+  aleatoireButtons.push(button);
+
+  button = new Button('Restriction\npar position',    0,y1,l,h);
+  button.button.onPress = function() {
+    solfegeMenu[1][0] = 1;
+    update(aleatoireButtons,1);
+  }
+  aleatoireButtons.push(button);
+
+  button = new Button('Restriction\npar tonalité',    l+e,y1,l,h);
+  button.button.onPress = function() {
+    solfegeMenu[1][0] = 2;
+    update(aleatoireButtons,2);
+  }
+  aleatoireButtons.push(button);
+
+  //----------------------------------------------------------------------------
+  //                           Gamme Buttons
+  //----------------------------------------------------------------------------
+
+  largeur(4);
+
+  button = new Button('Majeur',    -3*(l+e)/2,y1,l,h);
+  button.toggle(true);
+  button.button.onPress = function() {
+    solfegeMenu[1][1] = 0;
+    update(gammeButtons,0);
+  }
+  gammeButtons.push(button);
+
+  button = new Button('Mineur\nnaturel',     -  (l+e)/2,y1,l,h);
+  button.button.onPress = function() {
+    solfegeMenu[1][1] = 1;
+    update(gammeButtons,1);
+  }
+  gammeButtons.push(button);
+
+  button = new Button('Mineur\nharmonique',      (l+e)/2,y1,l,h);
+  button.button.onPress = function() {
+    solfegeMenu[1][1] = 2;
+    update(gammeButtons,2);
+  }
+  gammeButtons.push(button);
+
+  button = new Button('Mineur\nmélodique',3*(l+e)/2,y1,l,h);
+  button.button.onPress = function() {
+    solfegeMenu[1][1] = 3;
+    update(gammeButtons,3);
+  }
+  gammeButtons.push(button);
+
+  //----------------------------------------------------------------------------
+  //                           Rythme Buttons
+  //----------------------------------------------------------------------------
+
+  largeur(2);
+
+  button = new Button('Binaire',    -(l+e)/2,y1-(h+e)/4,l,(h-e)/2);
+  button.toggle(true);
+  button.button.onPress = function() {
+    solfegeMenu[1][2] = 0;
+    update(rythmeButtons,0,0,1);
+  }
+  rythmeButtons.push(button);
+
+  button = new Button('Ternaire',     (l+e)/2,y1-(h+e)/4,l,(h-e)/2);
+  button.button.onPress = function() {
+    solfegeMenu[1][2] = 1;
+    update(rythmeButtons,1,0,1);
+  }
+  rythmeButtons.push(button);
+
+  button = new Button('Lecture',      -(l+e)/2,y1+(h+e)/4,l,(h-e)/2);
+  button.toggle(true);
+  button.button.onPress = function() {
+    solfegeMenu[1][2][0] = 0;
+    update(rythmeButtons,0,2);
+  }
+  rythmeButtons.push(button);
+
+  button = new Button('Écriture',     (l+e)/2,y1+(h+e)/4,l,(h-e)/2);
+  button.button.onPress = function() {
+    solfegeMenu[1][2][0] = 2;
+    update(rythmeButtons,1,2);
+  }
+  rythmeButtons.push(button);
 
   //----------------------------------------------------------------------------
   //                           Cartes Buttons
   //----------------------------------------------------------------------------
 
-  button = new Button('Retour',    0,height/2.8,l,h/3);
+  largeur(3);
+
+  button = new Button('Retour',    -(l+e),yr,l,h/3);
   button.button.onPress = function() {
-      manager.showScene(homeScene);
+    manager.showScene(homeScene);
+  }
+  cartesButtons.push(button);
+
+  button = new Button('Jouer',    l+e,yr,l,h/3);
+  button.button.onPress = function() {
+    manager.showScene(solfegeGameScene);
   }
   cartesButtons.push(button);
 
@@ -151,9 +374,15 @@ function setup() {
   //                          Lutherie Buttons
   //----------------------------------------------------------------------------
 
-  button = new Button('Retour',    0,height/2.8,l,h/3);
+  button = new Button('Retour',    -(l+e),yr,l,h/3);
   button.button.onPress = function() {
-      manager.showScene(homeScene);
+    manager.showScene(homeScene);
+  }
+  lutherieButtons.push(button);
+
+  button = new Button('Jouer',    l+e,yr,l,h/3);
+  button.button.onPress = function() {
+    manager.showScene(solfegeGameScene);
   }
   lutherieButtons.push(button);
 
@@ -161,9 +390,15 @@ function setup() {
   //                        Compositeurs Buttons
   //----------------------------------------------------------------------------
 
-  button = new Button('Retour',    0,height/2.8,l,h/3);
+  button = new Button('Retour',    -(l+e),yr,l,h/3);
   button.button.onPress = function() {
-      manager.showScene(homeScene);
+    manager.showScene(homeScene);
+  }
+  compositeursButtons.push(button);
+
+  button = new Button('Jouer',    l+e,yr,l,h/3);
+  button.button.onPress = function() {
+    manager.showScene(solfegeGameScene);
   }
   compositeursButtons.push(button);
 }
@@ -176,10 +411,10 @@ function homeScene() {
   this.draw = function() {
     background(255);
 
-    fill(0);
+    fill(black);
     textAlign(CENTER,CENTER);
     textSize(40);
-    text("Nom de l'appliation",width/2,height/3.5);
+    text("Nom de l'appliation",width/2,yt);
 
     for(let b = 0; b < homeButtons.length; b++) {
       homeButtons[b].draw();
@@ -187,18 +422,41 @@ function homeScene() {
   }
 }
 
-function solfegeScene() {
+function solfegeMenuScene() {
   this.draw = function() {
     background(255);
 
-    fill(0);
+    fill(black);
     textAlign(CENTER,CENTER);
     textSize(40);
-    text("Solfège",width/2,height/3.5);
+    text("Solfège",width/2,yt);
 
     for(let b = 0; b < solfegeButtons.length; b++) {
       solfegeButtons[b].draw();
     }
+
+    var buttons = [];
+    switch(solfegeMenu[0]) {
+      case 0: buttons = aleatoireButtons; break;
+      case 1: buttons = gammeButtons; break;
+      case 2: buttons = rythmeButtons; break;
+    }
+    for(let b = 0; b < buttons.length; b++) {
+      buttons[b].draw();
+    }
+  }
+}
+
+function solfegeGameScene() {
+  this.draw = function() {
+    background(255);
+
+    fill(black);
+    textAlign(CENTER,CENTER);
+    textSize(40);
+    text("Not yet...",width/2,yt);
+    textSize(15);
+    text("(actualise la page)",width/2,2*yt);
   }
 }
 
@@ -206,10 +464,10 @@ function cartesScene() {
   this.draw = function() {
     background(255);
 
-    fill(0);
+    fill(black);
     textAlign(CENTER,CENTER);
     textSize(40);
-    text("Cartes",width/2,height/3.5);
+    text("Cartes",width/2,yt);
 
     for(let b = 0; b < cartesButtons.length; b++) {
       cartesButtons[b].draw();
@@ -221,10 +479,10 @@ function lutherieScene() {
   this.draw = function() {
     background(255);
 
-    fill(0);
+    fill(black);
     textAlign(CENTER,CENTER);
     textSize(40);
-    text("Lutherie",width/2,height/3.5);
+    text("Lutherie",width/2,yt);
 
     for(let b = 0; b < lutherieButtons.length; b++) {
       lutherieButtons[b].draw();
@@ -236,10 +494,10 @@ function compositeursScene() {
   this.draw = function() {
     background(255);
 
-    fill(0);
+    fill(black);
     textAlign(CENTER,CENTER);
     textSize(40);
-    text("Compositeurs",width/2,height/3.5);
+    text("Compositeurs",width/2,yt);
 
     for(let b = 0; b < compositeursButtons.length; b++) {
       compositeursButtons[b].draw();
